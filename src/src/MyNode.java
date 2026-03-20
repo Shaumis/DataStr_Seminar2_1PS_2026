0db1bc3cce6b4bd1c91695c74efcfaa45068361a
s25abelmiku <michisillproductions@gmail.com>
2026-03-20 18:36:38 +0200

    to String

--- src/src/MyNode.java
+++ src/src/MyNode.java
@@ -1,5 +1,38 @@
 package src;
 
 public class MyNode {
+	private char element;
+	private MyNode nextNode = null;
+	private MyNode previousNode = null;
+
+	public char getElement() {
+		return element;
+	}
+
+	public void setElement(char element) {
+		this.element = element;
+	}
+
+	public MyNode getNextNode() {
+		return nextNode;
+	}
+
+	public void setNextNode(MyNode nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public MyNode getPreviousNode() {
+		return previousNode;
+	}
+
+	public void setPreviousNode(MyNode previousNode) {
+		this.previousNode = previousNode;
+	}
+	public MyNode(char element) {
+		setElement(element);
+	}
+	public String toString() {
+		return "" + element;
+	}
 
 }
