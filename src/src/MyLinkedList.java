1c1e44372f4d85b341bdf0d29a190c04a5c8888f
s25abelmiku <michisillproductions@gmail.com>
2026-03-20 19:11:48 +0200

    added add function

--- src/src/MyLinkedList.java
+++ src/src/MyLinkedList.java
@@ -1,5 +1,41 @@
 package src;
 
 public class MyLinkedList {
+	private MyNode firstNode = null;
+	private MyNode lastNode = null;
+	private int howManyElements = 0;
 
+	public int getHowManyElements() {
+		return howManyElements;
+	}
+	public boolean isEmpty() {
+		return (howManyElements == 0);
+	}
+	public boolean isFull() {
+		try {
+		new MyNode('a');
+		return false;
+	}
+	catch(OutOfMemoryError error) {
+		return true;
+	}
+}
+	public void add(char element) throws Exception {
+		if(isFull()) {
+			throw new Exception("Saraksts ir pilnnns");
+		}
+		if(howManyElements == 0) {
+			MyNode newNode = new MyNode(element);
+			firstNode = newNode;
+			lastNode = newNode;
+			howManyElements++;
+		}
+		else
+		{
+			MyNode newNode = new MyNode(element);
+			newNode.setPreviousNode(lastNode);
+			lastNode.setNextNode(newNode);
+			lastNode = newNode;
+		}
+	}
 }
