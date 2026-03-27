aa7204aaba69fca369e60247809bf490fc0053df
s25abelmiku <michisillproductions@gmail.com>
2026-03-27 19:09:14 +0200

    bugsss

--- src/Service/MainService.java
+++ src/Service/MainService.java
@@ -1,24 +1,83 @@
 package Service;
 
 import src.MyLinkedList;
+import model.Student;
 
 public class MainService {
-
+//TODO no Sem1 paņemt studenta klasi un uztaisīt piemerus šeit arī ar stduentiem
 	public static void main(String[] args) {
-		MyLinkedList symbols = new MyLinkedList();
+		MyLinkedList<Character> symbols = new MyLinkedList<Character>();
 		try
 		{
-		symbols.add('l');
-		symbols.add('a');
-		symbols.add('d');
-		symbols.print();
-		symbols.add('Z',1);
-		symbols.print();
-		System.out.println(symbols.get(1));
-	}
-	catch (Exception e) {
-		System.out.println(e.getMessage());
-	}
+			symbols.add('B');
+			symbols.add('A');
+			symbols.add('C');
+			symbols.add('G');
+			symbols.print();//B A C G
+			System.out.println("Pievienot elementu sākumā");
+			symbols.add('Z',0);
+			symbols.print();//Z B A C G
+			System.out.println("Pieveinot elementu beigās");
+			symbols.add('W',5);
+			symbols.print();//Z B A C G W
+			System.out.println("Pievienot elementu pa vidu - 2.pozīcijā");
+			symbols.add('K', 2);
+			symbols.print(); //Z B A K C G W
+			
+			System.out.println("Izdzēšam elementu no sākuma");
+			symbols.remove(0);
+			symbols.print();//B A K C G W
+			
+			System.out.println("Izdzēšam elementu no beigām");
+			symbols.remove(5);
+			symbols.print();//B A K C G
+			
+			System.out.println("Izdzēšam elementu no vidus - 3. pozīcijā");
+			symbols.remove(3);
+			symbols.print();//B A K G
+			
+			System.out.println("Izgūt elementu no 2 pozīcijas");
+			System.out.println("2.pozīcijas element ir " + symbols.get(2));//A
+			
+			System.out.println("Atrast G elementa pozīciju");
+			System.out.println(symbols.search('G'));//3
+			
+			System.out.println("Iztukšot sarakstu un pēc tam pievineo jaunu elementu");
+			symbols.makeEmpty();
+			symbols.add('Q');
+			symbols.print();//Q
+
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("-----------------STUDENTI------------");
+		MyLinkedList<Student> allStudents = new MyLinkedList<Student>();
+			
+		try
+		{
+			allStudents.add(new Student("Janis", "Berzins"));//Janis
+			Student s1 = new Student("Baiba", "Jauka");
+			allStudents.add(s1);//Janis Baiba
+			allStudents.add(new Student("Peteris", "Nejaukais"), 0);//Peteris Janis Baiba
+			allStudents.print();//Peteris Janis Baiba
+			allStudents.remove(0);//Pēteris izdzest
+			allStudents.print();//Janis Baiba
+			allStudents.add(new Student("Kaspars", "Kalnins"));//Janis Baiba Kaspars
+			allStudents.add(new Student("Liga", "Jautra"));//Janis Baiba Kaspars Liga
+			System.out.println(allStudents.get(3));//Liga
+			System.out.println(allStudents.search(s1));//Mekleju Baibu - 1
+			allStudents.print();
+			allStudents.makeEmpty();
+			allStudents.add(new Student("Karina", "Skirmante"));
+			allStudents.print();//Karina
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
+
 }
--- src/src/MyLinkedList.java
+++ src/src/MyLinkedList.java
@@ -1,13 +1,17 @@
 package src;
 
-public class MyLinkedList {
-	private MyNode firstNode = null;
-	private MyNode lastNode = null;
-	private int howManyElements = 0;
+public class MyLinkedList<Ttype> {
+
+	private MyNode firstNode = null;// get un set netaisam, jo lieottajam nedodam iespēju tikt pie pašiem blokiem
+	private MyNode lastNode = null;// get un set netaisam, jo lieottajam nedodam iespēju tikt pie pašiem blokiem
+	private int howManyElements = 0;// set funkciju netaisam, jo neļaujam lietotjam pašam mainīt, cik daudz ir
+									// elementu
 
 	public int getHowManyElements() {
 		return howManyElements;
 	}
+
+	// nedefinēšu bezargumenta konstruktoru, oj izmantošanu Object mantoto
 
 	public boolean isEmpty() {
 		return (howManyElements == 0);
@@ -20,116 +24,210 @@ public class MyLinkedList {
 		} catch (OutOfMemoryError error) {
 			return true;
 		}
+
 	}
 
-	public void add(char element) throws Exception {
+	public void add(Ttype element) throws Exception {
 		if (isFull()) {
-			throw new Exception("Saraksts ir pilnnns");
+			throw new Exception("Saraksts ir pilns un nevar pievienot elementu");
 		}
-		if (howManyElements == 0) {
+
+		if(element == null) {
+			throw new Exception("Ievades elements nevar būt null");
+		}
+		
+		
+		// tiks pievienots pirmais elements un tam izveidots piramis bloks
+		if (isEmpty()) {
 			MyNode newNode = new MyNode(element);
 			firstNode = newNode;
 			lastNode = newNode;
 			howManyElements++;
-		} else {
+		}
+		// ja tiek pievienots otrais, tresais... elements
+		// TODO ja vēlas, tad var aoptimizet kodu, jo ir rindas, kuras sakrīt abos
+		// gadījumos
+		else {
 			MyNode newNode = new MyNode(element);
 			newNode.setPreviousNode(lastNode);
 			lastNode.setNextNode(newNode);
 			lastNode = newNode;
+			howManyElements++;
+
 		}
+
 	}
 
-	public void add(char element, int position) throws Exception {
+	public void add(Ttype element, int position) throws Exception {
+		if(element == null) {
+			throw new Exception("Ievades elements nevar būt null");
+		}
+		
+		if (isFull()) {
+			throw new Exception("Saraksts ir pilns un nevar pievienot elementu");
+		}
+		
 		if (position < 0) {
-			throw new Exception("Nevar būt mazāks par 0");
+			throw new Exception("Pozicīja nevar būt mazakā par 0");
 		}
+
 		if (position > howManyElements) {
-			throw new Exception("Too Big");
+			throw new Exception("Pozīcija ir lielāka kā atļautā");
 		}
+
+		// 1. ievietot sakumā kā nulto elementu
 		if (position == 0) {
 			MyNode newNode = new MyNode(element);
 			firstNode.setPreviousNode(newNode);
-			newNode.setNextNode(newNode);
-			howManyElements++;
-		} else if (position == howManyElements) {
-			add(element);
-		} else {
-			MyNode currentNode = firstNode;
-			for (int i = 1; i <= position; i++)
-				;
-			currentNode = currentNode.getNextNode();
-			MyNode previousNode = currentNode;
-			MyNode nextNode = currentNode.getNextNode();
-			MyNode newNode = new MyNode(element);
-			newNode.setPreviousNode(previousNode);
-			previousNode.setNextNode(newNode);
-			newNode.setNextNode(nextNode);
-			nextNode.setPreviousNode(newNode);
+			newNode.setNextNode(firstNode);
+			firstNode = newNode;
 			howManyElements++;
 		}
+
+		// 2. piekabināt beigās kā pēdejo elementu
+		else if (position == howManyElements) {
+			add(element);
+
+		}
+		// 3. iespraucināt elementu pa vidu
+		else {
+			MyNode currentNode = firstNode;// TODO noskaidrot, kurā pozicjā - tuvaak sākumam vai beigām ir jaievieto
+											// bloks
+			for (int i = 1; i < position; i++) {
+				currentNode = currentNode.getNextNode();
+			}
+
+			MyNode previousNode = currentNode;
+			MyNode nextNode = currentNode.getNextNode();
+
+			MyNode newNode = new MyNode(element);
+
+			newNode.setPreviousNode(previousNode);
+			previousNode.setNextNode(newNode);
+
+			newNode.setNextNode(nextNode);
+			nextNode.setPreviousNode(newNode);
+
+			howManyElements++;
+
+		}
+
+	}
+
+	public void print() throws Exception {
+		if (isEmpty()) {
+			throw (new Exception("Saraksts ir tukšs un to nevar izprintēt"));
+		}
+
+		MyNode currentNode = firstNode;
+		while (currentNode != null) {
+			System.out.print(currentNode.getElement() + " ");
+			currentNode = currentNode.getNextNode();
+		}
+		System.out.println();
+
 	}
 
 	public void remove(int position) throws Exception {
 		if (isEmpty()) {
-			throw (new Exception("Saraksts ir tukšs"));
-		}
-		if (position < 0) {
-			throw (new Exception("Nevar dzēst elementu, jo index ir negatīvs"));
+			throw (new Exception("Saraksts ir tukšs, tāpēc nevar izdzēst elementus"));
 		}
 
-		if (position > howManyElements) {
-			throw (new Exception("Nevar dzēst jaunu elementu, nav vietas!"));
+		if (position < 0) {
+			throw (new Exception("Nevar izdzēst elementu, jo pozīcija ir negatīva"));
 		}
+
+		if (position >= howManyElements) {
+			throw (new Exception("Nevar izdzēst elementu, jo pozīcija ir lielāks ka elementu skaits"));
+		}
+
+		// Pirmā bloka dzēšana
 		if (position == 0) {
 			MyNode newFirstNode = firstNode.getNextNode();
 			newFirstNode.setPreviousNode(null);
 			firstNode = newFirstNode;
 			howManyElements--;
-		} else if (position == howManyElements - 1) {
+		}
+		// dzēšam pēdējo bloku
+		else if (position == howManyElements - 1) {
 			MyNode newLastNode = lastNode.getPreviousNode();
 			newLastNode.setNextNode(null);
 			lastNode = newLastNode;
 			howManyElements--;
-		} else {
+		}
+		// dzēsam bloku pa vidu
+		else {
 			MyNode currentNode = firstNode;
 			for (int i = 1; i < position; i++) {
 				currentNode = currentNode.getNextNode();
 			}
+
 			MyNode newLeftNode = currentNode;
 			MyNode newRightNode = currentNode.getNextNode().getNextNode();
+
 			newLeftNode.setNextNode(newRightNode);
 			newRightNode.setPreviousNode(newLeftNode);
+
 			howManyElements--;
+
 		}
+
 	}
-	public char get(int position)throws Exception {
+	
+	public Ttype get(int position) throws Exception{
 		if (isEmpty()) {
-			throw (new Exception("Saraksts ir tukšs"));
+			throw (new Exception("Saraksts ir tukšs, tāpēc nevar izgūt elementus"));
 		}
+
 		if (position < 0) {
-			throw (new Exception("Nevar atrast elementu, jo index ir negatīvs"));
+			throw (new Exception("Nevar izgūt elementu, jo pozīcija ir negatīva"));
 		}
 
-		if (position > howManyElements) {
-			throw (new Exception("Nevar atrast jaunu elementu, nav vietas!"));
+		if (position >= howManyElements) {
+			throw (new Exception("Nevar izgūt elementu, jo pozīcija ir lielāka kā elementu skaits"));
 		}
+		
 		MyNode currentNode = firstNode;
-		for(int i = 1;i<position;i++) {
+		for(int i = 1; i <= position; i++) {
 			currentNode = currentNode.getNextNode();
-			
 		}
-		return currentNode.getElement();
+		
+		return (Ttype) currentNode.getElement();
+		
 	}
-
-	public void print() throws Exception {
+	
+	
+	//TODO mājās partaisīt funkciju, lai var atrast elmentu vairākas vietas un visas pozīcijas atgriež
+	public int search(Ttype element) throws Exception{
+		if(element == null) {
+			throw new Exception("Ievades elements nevar būt null");
+		}
+		
 		if (isEmpty()) {
-			throw (new Exception("Sarakstu nevar izveidot"));
+			throw (new Exception("Saraksts ir tukšs, tāpēc nevar meklēt elementus"));
 		}
+		
+			
+		int foundPosition = 0;
 		MyNode currentNode = firstNode;
-		while (currentNode != null) {
-			System.out.println(currentNode.getElement());
+		while(currentNode!=null) {
+			if(currentNode.getElement().equals(element)) {
+				return foundPosition;
+			}
+			foundPosition++;
 			currentNode = currentNode.getNextNode();
 		}
-		System.out.println();
+		
+		throw new Exception("Meklētais elements neeksistē sarakstā");
+		
 	}
+	
+	
+	public void makeEmpty() {
+		firstNode = null;
+		lastNode = null;
+		howManyElements = 0;
+		System.gc();
+	}
+
 }
