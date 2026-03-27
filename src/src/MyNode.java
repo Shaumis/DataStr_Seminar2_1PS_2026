5038e7bfde72e5b7fbbd85f4956ccdbf53f051bb
s25abelmiku <michisillproductions@gmail.com>
2026-03-27 18:27:17 +0200

    Uztaisija tType

--- src/src/MyNode.java
+++ src/src/MyNode.java
@@ -1,38 +1,41 @@
 package src;
 
-public class MyNode {
-	private char element;
+public class MyNode<Ttype> {
+	private Ttype element;
 	private MyNode nextNode = null;
 	private MyNode previousNode = null;
-
-	public char getElement() {
+	
+	public Ttype getElement() {
 		return element;
 	}
-
-	public void setElement(char element) {
-		this.element = element;
-	}
-
 	public MyNode getNextNode() {
 		return nextNode;
 	}
-
-	public void setNextNode(MyNode nextNode) {
-		this.nextNode = nextNode;
-	}
-
 	public MyNode getPreviousNode() {
 		return previousNode;
 	}
-
+	public void setElement(Ttype element) {
+		if(element != null)
+		{
+			this.element = element;
+		}
+		else
+		{
+			this.element = (Ttype) new Object();
+		}
+	}
+	public void setNextNode(MyNode nextNode) {
+		this.nextNode = nextNode;
+	}
 	public void setPreviousNode(MyNode previousNode) {
 		this.previousNode = previousNode;
 	}
-	public MyNode(char element) {
+	public MyNode(Ttype element) {
 		setElement(element);
 	}
+	
 	public String toString() {
 		return "" + element;
 	}
-
+	
 }
